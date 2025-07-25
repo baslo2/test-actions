ab3b48db01086d85f7480efdd0dad8020a34a7aa
Elmir Shamsutdinov <shamsutdinov_er@taximaxim.ru>
2025-07-25 11:04:42 +0300

    init

---
+++ src/main/java/test_action/Main.java
@@ -0,0 +1,9 @@
+package test_action;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.err.println(Utils.getMessage());
+    }
+
+}
---
+++ src/main/java/test_action/Utils.java
@@ -0,0 +1,11 @@
+package test_action;
+
+public class Utils {
+
+    private final static String MESSAGE = "{message}";
+
+    public static String getMessage() {
+        return MESSAGE;
+    }
+
+}
